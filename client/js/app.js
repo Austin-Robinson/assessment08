295a6706885331fc9b72a31e8ee78ee96b2ba8ee
Austin <robi1467@morris.umn.edu>
2022-08-08 09:40:43 -0500

    Making a getAgentCard

--- client/js/app.js
+++ client/js/app.js
@@ -10,25 +10,51 @@ function getAgents(){
         return response.json();
     })
 }
+function getAgentById(agentId){
+    return fetch('http://localhost:8080/api/agent/'+agentId)
+    .then(response => {
+        return response.json();
+    })
+}
 function renderList(agents){
     console.log(agents);
 
     const tableBodyElement = document.getElementById("tableRows");
     const agentHTML = agents.map(agent => {
-    return `
-    <tr>
-        <td>${agent.firstName} ${agent.middleName} ${agent.lastName}</td>
-        <td>${agent.dob}</td>
-        <td>${agent.heightInInches}</td>
-        <td>
-            <button onclick="handleUpdate(${agent.agentId})" class="btn btn-primary">update</button>
-            <button onclick="handleDelete(${agent.agentId})" class="btn btn-danger">delete</button>
-        </td>
-    </tr>
-    `;
+        let dob = agent.dob;
+        if(dob == null || dob == ""){
+            dob="REDACTED";
+        }
+        return `
+        <tr>
+            <td id="nameRow"  onclick="getAgentCard(${agent.agentId})">${agent.firstName} ${agent.middleName} ${agent.lastName} </td>
+            <td>${dob}</td>
+            <td>${agent.heightInInches}</td>
+            <td>
+                <button onclick="handleUpdate(${agent.agentId})" class="btn btn-primary">update</button>
+                <button onclick="handleDelete(${agent.agentId})" class="btn btn-danger">delete</button>
+            </td>
+        </tr>
+        `;
     });
     tableBodyElement.innerHTML = agentHTML.join('');
 
+}
+function getAgentCard(agentId){
+    getAgentById(agentId)
+    .then(agent =>{
+        const fullName = agent.firstName + " " + agent.middleName + " " + agent.lastName;
+        const height = agent.heightInInches;
+        let DOB = agent.dob;
+        
+        if(DOB == null){
+            DOB ="REDACTED";
+        }
+        const message = "Name: "+ fullName + "\nDOB: " + DOB +"\nHeight in Inches: " + height + "\nAliases: " + agent.aliases + "\nAgencies: " + agent.agencies;
+        alert(message);
+    }
+    );
+    
 }
 function handleUpdate(agentId){
     alert("Agent ID: " + agentId);    
